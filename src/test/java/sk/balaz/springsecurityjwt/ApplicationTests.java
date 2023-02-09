fdf70f9ebf18f65578db5783c4d1e87e977c486c
Tomas Balaz <balaztomas0@gmail.com>
2023-02-09 15:19:30 +0100

    Refactor SpringSecurityJwtApplication to Application

--- src/test/java/sk/balaz/springsecurityjwt/ApplicationTests.java
+++ src/test/java/sk/balaz/springsecurityjwt/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringSecurityJwtApplicationTests {
+class ApplicationTests {
 
   @Test
   void contextLoads() {
