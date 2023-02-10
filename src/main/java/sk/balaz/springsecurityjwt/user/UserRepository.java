59f7e2e5f625da6f1df8d30302133d34b785f1ba
Tomas Balaz <balaztomas0@gmail.com>
2023-02-10 15:39:24 +0100

    Created UserRepository

---
+++ src/main/java/sk/balaz/springsecurityjwt/user/UserRepository.java
@@ -0,0 +1,11 @@
+package sk.balaz.springsecurityjwt.user;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+  Optional<User> findByEmail(String email);
+}
