078cf26be45891c3067ba6afd5cff5feb7f69689
Tomas Balaz <balaztomas0@gmail.com>
2023-02-10 15:20:56 +0100

    Annotated User class as Spring Data JPA Entity

--- src/main/java/sk/balaz/springsecurityjwt/user/User.java
+++ src/main/java/sk/balaz/springsecurityjwt/user/User.java
@@ -1,12 +1,24 @@
 package sk.balaz.springsecurityjwt.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
 public class User {
 
+  @Id
+  @GeneratedValue
   private Integer id;
   private String fistName;
   private String lastName;
