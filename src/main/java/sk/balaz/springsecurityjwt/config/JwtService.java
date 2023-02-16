fbeea3e4492e19108a3660949a73e54f86f56500
Tomas Balaz <balaztomas0@gmail.com>
2023-02-16 15:47:21 +0100

    Created isTokenValid method

--- src/main/java/sk/balaz/springsecurityjwt/config/JwtService.java
+++ src/main/java/sk/balaz/springsecurityjwt/config/JwtService.java
@@ -66,6 +66,19 @@ public class JwtService {
 
   }
 
+  public boolean isTokenValid(String token, UserDetails userDetails) {
+    String userName = extractUserName(token);
+    return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+  }
+
+  private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  private Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
+  }
+
   private Claims extractAllClaims(String token) {
     return Jwts
         .parserBuilder()
