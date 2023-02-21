1945b11624b10bbccb092748ef048b3f909e7f58
Tomas Balaz <balaztomas0@gmail.com>
2023-02-21 15:43:31 +0100

    Created AuthenticationProvider bean with PasswordEncoder bean

--- src/main/java/sk/balaz/springsecurityjwt/config/ApplicationConfig.java
+++ src/main/java/sk/balaz/springsecurityjwt/config/ApplicationConfig.java
@@ -3,8 +3,12 @@ package sk.balaz.springsecurityjwt.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import sk.balaz.springsecurityjwt.user.UserRepository;
 
 @Configuration
@@ -18,5 +22,17 @@ public class ApplicationConfig {
 
     return username -> userRepository.findByEmail(username)
         .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  }
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setUserDetailsService(userDetailsService());
+    provider.setPasswordEncoder(passwordEncoder());
+    return provider;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
